cfa8fe2bdf14c3b9e3503792f704904592da92d6
Ayushgunjal2025 <ayushgunjal2025@gmail.com>
2025-07-03 18:42:34 +0530

    Initial API for both models

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+# Load both models
+simple_model = joblib.load("model_simple.joblib")
+multi_model = joblib.load("model_multi.joblib")
+
+@app.route("/")
+def home():
+    return "API is live!"
+
+@app.route("/predict/package")
+def predict_package():
+    try:
+        cgpa = float(request.args.get("cgpa", 0))
+        result = simple_model.predict([[cgpa]])[0]
+        return jsonify(package=round(result, 2))
+    except:
+        return jsonify(error="Invalid CGPA")
+
+@app.route("/predict/salary")
+def predict_salary():
+    try:
+        age = float(request.args.get("age"))
+        exp = float(request.args.get("exp"))
+        result = multi_model.predict([[age, exp]])[0]
+        return jsonify(salary=round(result, 2))
+    except:
+        return jsonify(error="Invalid age or experience")
+
+if __name__ == "__main__":
+    app.run()
