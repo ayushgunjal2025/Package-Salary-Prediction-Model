7102af56819b69383d622e19e722206c512ab59e
Ayushgunjal2025 <ayushgunjal2025@gmail.com>
2025-07-03 19:21:44 +0530

    Initial API for both models

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -30,5 +31,8 @@ def predict_salary():
     except:
         return jsonify(error="Invalid age or experience")
 
+
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
